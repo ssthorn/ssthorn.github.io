956f0afbb2fa6098a4b3880974657ab95ef471fc
Sam <samthornton@StarBook4ee916a9.lan>
2022-02-25 15:58:18 -0800

    mouse effects

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+$(document).ready(function(){
+  $("li").on({
+      mouseover: function(){
+          $(this).css("background-color", "white");
+      },  
+     
+      mouseleave: function(){
+          $(this).css("background-color", "rgb(10, 57, 73)");
+      },
+     
+      mousedown: function(){
+          $(this).css("background-color", "#cbf2a8");
+      },
+
+      mouseup: function(){
+        $(this).css("background-color", "white");
+      },
+      
+   
+    
+});
+
+
+
+
+
+
+});

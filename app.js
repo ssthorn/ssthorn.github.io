7efb7eabc2ccd8377b1cc695f5c1fe5c396eb611
Sam <samthornton@StarBook4ee916a9.lan>
2022-02-25 17:52:56 -0800

    adding sliding tabs feature

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 $(document).ready(function(){
   $("li").on({
       mouseover: function(){
-          $(this).css("background-color", "white");
+          $(this).css("background-color", "rgb(10, 57, 73)");
       },  
      
       mouseleave: function(){
@@ -19,13 +19,27 @@ $(document).ready(function(){
       },
 
       mouseup: function(){
-        $(this).css("background-color", "white");
+        $(this).css("background-color", "rgb(10, 57, 73)");
       },
-      
-   
-    
 });
 
+$("#projects").on("click", function(){
+    console.log("projects has been clicked")
+    $(".projects1").toggleClass("projects1-open");
+});
+$("#projects1-close").on("click", function() {
+    console.log("close thingy clicked");
+    $(".projects1").removeClass("projects1-open");
+});
+
+$("#resume").on("click", function(){
+    console.log("resume has been clicked")
+    $(".resume1").toggleClass("resume1-open");
+});
+$("#projects1-close").on("click", function() {
+    console.log("close thingy clicked");
+    $(".resume1").removeClass("resume1-open");
+});
 
 
 

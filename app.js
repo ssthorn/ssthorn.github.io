ca1ee8b0bcfc58e6a1ca62f15c9d88f39d4dd157
Sam <samthornton@StarBook4ee916a9.lan>
2022-03-01 15:05:57 -0800

    adding content. working on nav

--- app.js
+++ app.js
@@ -6,6 +6,61 @@ $('.fruitty:even').css("background-image", "url(https://cdn.britannica.com/40/75
 $('#menu').click(function(){
         console.log('menu clicked')
     $('#hidden-nav').toggleClass("open");
+    
 });
+
+$('#to-resume').click(function(){
+    console.log('to-resume clicked')
+$('#resume').toggleClass("open");
+$('#about').removeClass("open");
+$('#contact').removeClass("open");
+$('#links').removeClass("open");
+
+});
+
+$('#to-about').click(function(){
+    console.log('to-about clicked')
+$('#about').toggleClass("open");
+$('#contact').removeClass("open");
+$('#links').removeClass("open");
+$('#resume').removeClass("open");
+
+});
+
+$('#to-contact').click(function(){
+    console.log('to-contact clicked')
+$('#contact').toggleClass("open");
+$('#links').removeClass("open");
+$('#resume').removeClass("open");
+$('#about').removeClass("open");
+$('html, body').animate({ scrollBottom: 0 }, 'fast');
+
+});
+
+$('#to-links').click(function(){
+    console.log('to-links clicked')
+$('#links').toggleClass("open");
+$('#resume').removeClass("open");
+$('#about').removeClass("open");
+$('#contact').removeClass("open");
+
+});
+
+$('#nametag').click(function(){
+    $('.open').removeClass("open");
+    $('html, body').animate({ scrollTop: 0 }, 'fast');
+
+
+
+});
+
+
+
+
+
+
+
+
+
 
 });

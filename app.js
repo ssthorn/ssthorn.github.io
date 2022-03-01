ddd3039c3fee77b6499ece18a9bb66019db0aa22
Sam <samthornton@StarBook4ee916a9.lan>
2022-02-28 23:28:27 -0800

    design rework

--- app.js
+++ app.js
@@ -1,82 +1,11 @@
+$(()=>{
 
+$('.fruitty:odd').css("background-image", "url(https://cdn.britannica.com/29/150929-050-547070A1/lion-Kenya-Masai-Mara-National-Reserve.jpg)");
+$('.fruitty:even').css("background-image", "url(https://cdn.britannica.com/40/75640-050-F894DD85/tiger-Siberian.jpg)");
 
-
-
-
-
-$(document).ready(function(){
-  $(".button").on({
-   
-      mouseleave: function(){
-          $(this).css("color", "white");
-      },
-     
-      mousedown: function(){
-          $(this).css("color", "#cbf2a8");
-      },
-
-      mouseup: function(){
-         $(this).css("color", "white");
-      },
+$('#menu').click(function(){
+        console.log('menu clicked')
+    $('#hidden-nav').toggleClass("open");
 });
-
-$("#projects").on("click", function(){
-    $(".projects1").toggleClass("projects1-open");
-    $(".resume1").removeClass("resume1-open");
-    $(".about1").removeClass("about1-open");
-    $(".links1").removeClass("links1-open");
-    $(".contact").removeClass("dot-open");
-});
-
-$("#resume").on("click", function(){
-    $(".resume1").toggleClass("resume1-open");
-    $(".about1").removeClass("about1-open");
-    $(".links1").removeClass("links1-open");
-    $(".projects1").removeClass("projects1-open");
-    $(".contact").removeClass("dot-open");
-});
-
-
-$("#about").on("click", function(){
-    $(".about1").toggleClass("about1-open");
-    $(".resume1").removeClass("resume1-open");
-    $(".projects1").removeClass("projects1-open");
-    $(".links1").removeClass("links1-open");
-    $(".contact").removeClass("dot-open");
-});
-
-
-$("#links").on("click", function(){
-    $(".links1").toggleClass("links1-open");
-    $(".about1").removeClass("about1-open");
-    $(".resume1").removeClass("resume1-open");
-    $(".projects1").removeClass("projects1-open");
-    $(".contact").removeClass("dot-open");
-});
-
-$("#dot").on({
-    mouseup: function(){
-        console.log("dot click");
-        $(".contact").toggleClass("dot-open");
-        $(".links1").removeClass("links1-open");
-        $(".about1").removeClass("about1-open");
-        $(".resume1").removeClass("resume1-open");
-        $(".projects1").removeClass("projects1-open");   
-    },
-   
-});
-
-$(".responsive-header h1").on("click", function(){
-    window.location = 'index.html'
-
-})
-
-
-
-
-
-
-
-
 
 });
